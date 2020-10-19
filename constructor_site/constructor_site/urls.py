cd68e413302edc30a3e2dd612229bc33d30b1c6e
Nurdaulet002 <73096695+Nurdaulet002@users.noreply.github.com>
2020-10-19 16:09:31 +0600

    I change something

--- constructor_site/constructor_site/urls.py
+++ constructor_site/constructor_site/urls.py
@@ -17,8 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('management_site.urls', namespace='management_site')),
+    path('login',auth_views.LoginView.as_view(),name='login'),
+    path('logout',auth_views.LogoutView.as_view(),name='logout'),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, 
+		                 document_root=settings.MEDIA_ROOT)
--- constructor_site/management_site/models.py
+++ constructor_site/management_site/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Список сайтов
 class Site(models.Model):
     title = models.CharField(max_length=120)
-    
-    class Meta:
-        db_table='site'
-
     def __str__(self):
     	return self.title
+
+
+class UserSite(models.Model):
+    user = models.ForeignKey(User, related_name='user_related', on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, related_name='site_related', on_delete=models.CASCADE)
+    def __str__(self):
+    	return self.user
