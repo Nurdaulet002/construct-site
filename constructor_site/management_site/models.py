4774e2608e1236f37856f424eda5aa76d7957af0
sayat-nokhaos <73094617+sayat-nokhaos@users.noreply.github.com>
2020-10-19 13:34:38 +0600

    сайт модель костым

--- constructor_site/management_site/models.py
+++ constructor_site/management_site/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Список сайтов
+class Site(models.Model):
+    title = models.CharField(max_length=120)
+    
+    class Meta:
+        db_table='site'
+
+    def __str__(self):
+    	return self.title
